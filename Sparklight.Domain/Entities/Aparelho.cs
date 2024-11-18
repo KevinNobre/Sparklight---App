962da2942a96d30180703c48e636fbf73c2ab4e0
KevinNobre <kevinchristian.nobre@gmail.com>
2024-11-18 01:26:52 -0300

    ✨ feat: Criando Entidades Usuario e Aparelho

---
+++ Sparklight.Domain/Entities/Aparelho.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sparklight.Domain.Entities
+{
+    [Table("tb_aparelho")]
+    public class Aparelho
+    {
+        [Key]
+        [Column("imagem_id")]
+        public int AparelhoId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [Column("potencia")]
+        public double Potencia { get; set; }
+
+        [Required]
+        [Column("tempo")]
+        public int Tempo { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [Column("periodo")]
+        public string Periodo { get; set; }
+
+        [ForeignKey("tb_usuario")]
+        [Column("usuario_id")]
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public Aparelho()
+        { }
+
+    }
+}
---
+++ Sparklight.Domain/Entities/Usuario.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sparklight.Domain.Entities
+{
+    [Table("tb_usuario")]
+    public class Usuario
+    {
+        [Key]
+        [Column("usuario_id")]
+        public int UsuarioId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("sobrenome")]
+        public string Sobrenome { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        [Column("cep")]
+        public string CEP { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("estado")]
+        public string Estado { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Column("cidade")]
+        public string Cidade { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        [Column("genero")]
+        public string Genero { get; set; }
+
+        [Required]
+        [Column("idade")]
+        public int Idade { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        [Column("contato")]
+        public string Contato { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [Column("senha")]
+        public string Senha { get; set; }
+
+        public Usuario()
+        { }
+
+    }
+}
