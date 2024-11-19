8f4c6dc754eacc106c3f403c8ae3bfcd2df8b428
KevinNobre <kevinchristian.nobre@gmail.com>
2024-11-19 12:45:12 -0300

    ✨ feat: Criando LoginPage, estilizando e configurando as rotas

--- Sparklight/Controllers/HomeController.cs
+++ Sparklight/Controllers/HomeController.cs
@@ -18,6 +18,27 @@ namespace Sparklight.Controllers
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string Email, string Password)
+        {
+            
+            if (Email == "admin@sparklight.com" && Password == "123456")
+            {
+                TempData["UserName"] = "Admin"; 
+                return RedirectToAction("Dashboard");
+            }
+
+            // Caso as credenciais sejam inválidas
+            ViewBag.ErrorMessage = "Credenciais inválidas. Tente novamente.";
+            return View("Index");
+        }
+
+        public IActionResult Dashboard()
+        {
+            ViewBag.UserName = TempData["UserName"] ?? "Usuário";
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
